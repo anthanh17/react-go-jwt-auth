9c953d967fe4ebbba1d20df8ab4d893408ef6614
Annt <annguyenthe.work@gmail.com>
2022-08-20 21:36:52 +0700

    add func User: login when client send cookie - jwt

--- backend/controllers/authController.go
+++ backend/controllers/authController.go
@@ -55,12 +55,12 @@ func Login(c *fiber.Ctx) error {
 	}
 
 	// passwd successfully
-	// create token
+	// create payload gen token to user.Id
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
 		Issuer:    strconv.Itoa(int(user.Id)),
 		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 1 day
 	})
-
+	// create signture to SecrerKey (server)
 	token, err := claims.SignedString([]byte(SecretKey))
 
 	if err != nil {
@@ -69,7 +69,7 @@ func Login(c *fiber.Ctx) error {
 			"message": "Could not login",
 		})
 	}
-
+	// create cookie store token
 	cookie := fiber.Cookie{
 		Name:     "jwt",
 		Value:    token,
@@ -77,8 +77,31 @@ func Login(c *fiber.Ctx) error {
 		HTTPOnly: true,
 	}
 	c.Cookie(&cookie)
-	return c.JSON(token)
-	// return c.JSON(fiber.Map{
-	// 	"message": "success",
-	// })
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
 }
